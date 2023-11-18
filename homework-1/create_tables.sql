f1a68a86fcaa255008b09c43cb42b3b3e0ce5939
vitalinho <kirillovitaly@mail.ru>
2023-11-18 18:50:34 +0300

    add code to create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id int PRIMARY KEY,
+	company_name text NOT NULL,
+	contact_name text NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id int REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date,
+	ship_city varchar(100) NOT NULL
+);
+
+
+SELECT * FROM orders;
