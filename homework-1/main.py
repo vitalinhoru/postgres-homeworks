0f919d914298aeb171b077f03f9021375b9f7026
vitalinho <kirillovitaly@mail.ru>
2023-11-18 19:53:17 +0300

    написан скрипт в main.py

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,54 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+import datetime
+
+# заполняем таблицу employees
+with open('north_data/employees_data.csv', 'r', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='1234')
+        try:
+            with conn:
+                with conn.cursor() as cur:
+                    cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)',
+                                (row['employee_id'],
+                                 row['first_name'],
+                                 row['last_name'],
+                                 row['title'],
+                                 datetime.datetime.strptime(row['birth_date'], '%Y-%m-%d'),
+                                 row['notes']))
+        finally:
+            conn.close()
+
+# заполняем таблицу customers
+with open('north_data/customers_data.csv', 'r', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='1234')
+        try:
+            with conn:
+                with conn.cursor() as cur:
+                    cur.execute('INSERT INTO customers VALUES (%s, %s, %s)',
+                                (row['customer_id'],
+                                 row['company_name'],
+                                 row['contact_name']))
+        finally:
+            conn.close()
+
+# заполняем таблицу orders
+with open('north_data/orders_data.csv', 'r', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='1234')
+        try:
+            with conn:
+                with conn.cursor() as cur:
+                    cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)',
+                                (row['order_id'],
+                                 row['customer_id'],
+                                 row['employee_id'],
+                                 row['order_date'],
+                                 row['ship_city']))
+        finally:
+            conn.close()
